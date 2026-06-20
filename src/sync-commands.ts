ff0cfa4a5ba5a56a4412e2e86c874d36829af0a9
Matt Hatcher <3768988+MatthewSH@users.noreply.github.com>
2026-06-20 05:55:57 -0400

    Use GUILD_ID when syncing commands

--- src/sync-commands.ts
+++ src/sync-commands.ts
@@ -3,5 +3,7 @@ import { commands } from "./commands";
 import { env } from "./env";
 
 const rest = new REST({ version: "10" }).setToken(env.discordToken);
-await rest.put(Routes.applicationCommands(env.clientId), { body: commands });
+const endpoint = env.guildId === null ? Routes.applicationCommands(env.clientId) : Routes.applicationGuildCommands(env.clientId, env.guildId);
+
+await rest.put(endpoint, { body: commands });
 console.log(`Synced ${commands.length} command group(s).`);
